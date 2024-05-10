b1c7fdbe95991a4a30f2d589b622f484d39cd7d8
Andrew Longosz <alongosz@users.noreply.github.com>
2024-05-10 11:45:51 +0200

    IBX-8118: Implemented RemoveLegacyClassAliasRector rule (#2)
    For more details see https://issues.ibexa.co/browse/IBX-8118 and https://github.com/ibexa/rector/pull/2
    
    * [Rector] Defined internal rule to remove legacy class aliases
    
    * [Tests] Added coverage for the rule to remove class aliases

---
+++ tests/lib/Rule/Internal/RemoveLegacyClassAliasRector/config/configured_rule.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * @copyright Copyright (C) Ibexa AS. All rights reserved.
+ * @license For full copyright and license information view LICENSE file distributed with this source code.
+ */
+declare(strict_types=1);
+
+use Ibexa\Rector\Rule\Internal\RemoveLegacyClassAliasRector;
+use Rector\Config\RectorConfig;
+
+return static function (RectorConfig $rectorConfig): void {
+    $rectorConfig->rule(RemoveLegacyClassAliasRector::class);
+};
