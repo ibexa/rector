f4d9f5f8cdc66ca7a952e721df63d11bf320e91c
Dawid Parafinski <dawid.parafinski@ibexa.co>
2024-06-17 16:37:56 +0200

    Remove method calls as well

--- src/lib/Rule/Internal/RemoveInterfaceWithMethods.php
+++ src/lib/Rule/Internal/RemoveInterfaceWithMethods.php
@@ -9,8 +9,12 @@ declare(strict_types=1);
 namespace Ibexa\Rector\Rule\Internal;
 
 use PhpParser\Node;
+use PhpParser\Node\Expr\MethodCall;
 use PhpParser\Node\Stmt\ClassMethod;
+use PhpParser\NodeTraverser;
+use PhpParser\NodeVisitorAbstract;
 use Rector\Contract\Rector\ConfigurableRectorInterface;
+use Rector\NodeNameResolver\NodeNameResolver;
 use Rector\Rector\AbstractRector;
 use Rector\Removing\Rector\Class_\RemoveInterfacesRector;
 use ReflectionClass;
@@ -78,17 +82,59 @@ final class RemoveInterfaceWithMethods extends AbstractRector implements Configu
                 foreach ($this->interfacesToRemove as $interface) {
                     $ref = new ReflectionClass($interface);
                     $methods = array_map(static fn (ReflectionMethod $reflectionMethod) => $reflectionMethod->getName(), $ref->getMethods());
-                    foreach ($methods as $methodToRemove) {
-                        foreach ($node->stmts as $key => $stmt) {
-                            if ($stmt instanceof ClassMethod && $this->isName($stmt, $methodToRemove)) {
-                                unset($node->stmts[$key]);
+
+                    // Remove method definition
+                    foreach ($node->stmts as $key => $stmt) {
+                        if ($stmt instanceof ClassMethod && $this->isNames($stmt, $methods)) {
+                            unset($node->stmts[$key]);
+                        }
+                    }
+
+                    // Remove method calls, if one of the arguments was removed method
+                    foreach ($node->getMethods() as $classMethod) {
+                        $nodeTraverser = new NodeTraverser();
+                        $nodeTraverser->addVisitor(new class($this->nodeNameResolver, $methods) extends NodeVisitorAbstract {
+                            private NodeNameResolver $nodeNameResolver;
+
+                            /** @var string[] */
+                            private array $methods;
+
+                            /**
+                             * @param string[] $methods
+                             */
+                            public function __construct(NodeNameResolver $nodeNameResolver, array $methods)
+                            {
+                                $this->nodeNameResolver = $nodeNameResolver;
+                                $this->methods = $methods;
                             }
+
+                            public function leaveNode(Node $node)
+                            {
+                                if ($node instanceof MethodCall) {
+                                    foreach ($node->getArgs() as $arg) {
+                                        $argValue = $arg->value;
+                                        if ($argValue instanceof MethodCall && $this->nodeNameResolver->isNames($argValue->name, $this->methods)) {
+                                            return $node->var;
+                                        }
+                                    }
+                                }
+
+                                return null;
+                            }
+                        });
+
+                        if ($classMethod->stmts !== null) {
+                            /** @var array<\PhpParser\Node\Stmt>|null $traversedStmts */
+
+                            $traversedStmts = $nodeTraverser->traverse($classMethod->stmts);
+                            $classMethod->stmts = $traversedStmts;
                         }
                     }
                 }
             }
         }
 
+        // Remove interface
         $this->removeInterfacesRector->refactor($node);
 
         return null;
