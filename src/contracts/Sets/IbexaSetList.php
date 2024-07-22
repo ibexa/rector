dc0183619b83a18b20b8cb1b864a678efe7e60b9
Andrew Longosz <alongosz@users.noreply.github.com>
2024-07-22 14:39:26 +0200

    Added RepositoryConfigurationProviderInterface renaming rule config

---
+++ src/contracts/Sets/IbexaSetList.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * @copyright Copyright (C) Ibexa AS. All rights reserved.
+ * @license For full copyright and license information view LICENSE file distributed with this source code.
+ */
+declare(strict_types=1);
+
+namespace Ibexa\Contracts\Rector\Sets;
+
+enum IbexaSetList: string
+{
+    case IBEXA_50 = __DIR__ . '/ibexa-50.php';
+}
