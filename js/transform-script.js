cf3a68521d71c528fa45bfcc617c1ab8aaa0bba7
Michał Grabowski <michal.grabowski@ibexa.co>
2024-08-01 11:47:14 +0200

    JS babel transformers

---
+++ js/transform-script.js
@@ -0,0 +1,31 @@
+const { execSync } = require('child_process');
+const path = require('path');
+const fs = require('fs');
+
+const prettierConfigFile = require.resolve('eslint-config-ibexa/prettier');
+const DEFAULT_INPUT_DIR = 'src/bundle/Resources/public,src/bundle/ui-dev/src/modules';
+const { INIT_CWD, INPUT, OUTPUT, RUN_ESLINT } = process.env;
+const inputValue = INPUT ?? DEFAULT_INPUT_DIR;
+const outputValue = OUTPUT ?? DEFAULT_INPUT_DIR;
+const inputPaths = inputValue.split(',');
+const outputPaths = outputValue.split(',');
+
+inputPaths.forEach((inputPath, index) => {
+    const inputAbsolutePath = path.join(INIT_CWD, inputPath);
+    const outputAbsolutePath = outputPaths[index] ? path.join(INIT_CWD, outputPaths[index]) : inputAbsolutePath;
+
+    if (!fs.existsSync(inputAbsolutePath)) {
+        return;
+    }
+
+    let command = `babel ${inputAbsolutePath} -d ${outputAbsolutePath} --retain-lines`;
+    command += ` && yarn prettier "${outputAbsolutePath}/**/*.js" --config ${prettierConfigFile} --write`;
+
+    if (RUN_ESLINT === '1') {
+        command += ` && yarn eslint "${outputAbsolutePath}/**/*.js" --fix`;
+    }
+
+    try {
+        execSync(command, { stdio: 'inherit' });
+    } catch (err) {} // eslint-disable-line no-empty
+});
