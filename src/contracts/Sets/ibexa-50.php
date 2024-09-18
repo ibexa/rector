e6585b2903eab9d6ce9a693a7a4f879294950c3e
Dawid Parafinski <dawid.parafinski@ibexa.co>
2024-09-18 15:35:23 +0200

    IBX-8534: Changed ContentService method usage

--- src/contracts/Sets/ibexa-50.php
+++ src/contracts/Sets/ibexa-50.php
@@ -69,6 +69,11 @@ return static function (RectorConfig $rectorConfig): void {
                 'serializeFieldValue',
                 'serializeContentFieldValue'
             ),
+            new MethodCallRename(
+                'Ibexa\Contracts\Core\Repository\ContentService',
+                'loadContentDrafts',
+                'loadContentDraftList'
+            ),
         ]
     );
 
