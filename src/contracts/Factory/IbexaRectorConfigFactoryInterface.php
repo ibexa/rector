e62023f0f0c77e1112ddd7cf086bd3d2277e4ac8
Andrew Longosz <alongosz@users.noreply.github.com>
2025-02-24 18:34:58 +0100

    Implemented IbexaRectorConfigFactory

---
+++ src/contracts/Factory/IbexaRectorConfigFactoryInterface.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * @copyright Copyright (C) Ibexa AS. All rights reserved.
+ * @license For full copyright and license information view LICENSE file distributed with this source code.
+ */
+declare(strict_types=1);
+
+namespace Ibexa\Contracts\Rector\Factory;
+
+use Rector\Configuration\RectorConfigBuilder;
+
+/**
+ * Provides configuration contract to generate RectorConfigBuilder with Ibexa set configuration for internal use by Ibexa packages.
+ */
+interface IbexaRectorConfigFactoryInterface
+{
+    public function __invoke(): RectorConfigBuilder;
+}
