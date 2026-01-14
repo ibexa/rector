a2f35bbccd27a87c85e48981cb64114eb126e91b
Andrew Longosz <alongosz@users.noreply.github.com>
2026-01-14 15:28:49 +0100

    [Tests] Added coverage for Ibexa annotation to attribute rector config

---
+++ stubs/Ibexa/Payment/Validation/Constraints/UniquePaymentMethodIdentifier.php
@@ -0,0 +1,11 @@
+<?php
+
+
+namespace Ibexa\Payment\Validation\Constraints;
+
+use Symfony\Component\Validator\Constraint;
+
+#[\Attribute(\Attribute::TARGET_PROPERTY | \Attribute::TARGET_METHOD)]
+final class UniquePaymentMethodIdentifier extends Constraint
+{
+}
---
+++ stubs/Ibexa/Segmentation/Validation/Constraint/UserToSegmentAssignment.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Ibexa\Segmentation\Validation\Constraint;
+
+use Symfony\Component\Validator\Constraint;
+
+#[\Attribute(\Attribute::TARGET_CLASS)]
+final class UserToSegmentAssignment extends Constraint
+{
+
+}
